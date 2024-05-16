c2694aa01815966b46cdf3455628dc046eebd146
Your Name <you@example.com>
2024-05-16 19:09:23 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -154,6 +154,7 @@ Node* DFS(Node* initial, int* cont)
             {
                push(s,aux);
                aux = next(list);
+               cont++;
             }
          free(n);
       }

90464ece2bf0a53b33f9ab0bcc9b098e4a4e0049
Your Name <you@example.com>
2024-05-16 17:39:54 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -62,6 +62,7 @@ List* get_adj_nodes(Node* n)
             Node *adj = copy(n);
             adj->sudo[i][j] = contador;
             pushBack(list, adj);
+            adj = copy(n);
             contador++;
          }
    
